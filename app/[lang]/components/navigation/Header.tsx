d1f972bb89cdeb7c5cf89b74932f65565865c264
vineela <vineela.ampili@technoforte.co.in>
2023-10-05 15:07:38 +0530

    updating pictures

--- app/[lang]/components/navigation/Header.tsx
+++ app/[lang]/components/navigation/Header.tsx
@@ -7,24 +7,24 @@ import { Search } from '..'
 import ProfileDropDown from './ProfileDropdown'
 
 
-export default function Header() {
-
+export default  function Header() {
+  
   return (
     <header >
       <nav className="w-full font-fontcustom bg-no-repeat shadow-md opacity-100   bg-brand border-gray-200 p-1 lg:px-4  ">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl ">
           <Link href="/" className=" m-3 flex items-center">
             <Image
-              src="/img/person.png" // Relative path to the image from your project root
+              src="/img/logo@2x.png" // Relative path to the image from your project root
               alt="Logo"
-              width={100}
-              height={50}
+              width={300}
+              height={300}
             />
           </Link>
-
+                    
           <div className="flex items-center gap-2 lg:order-2 mr-6">
             <Search />
-            <LanguageDropDown />
+            <LanguageDropDown/>
             <ProfileDropDown />
           </div>
         </div>
