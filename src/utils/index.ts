e03a1a2e583659bcea2b39cd5c76892585acdd18
shivamg9 <shivamg@iitkgp@ac.in>
2024-03-15 18:17:55 +0530

    Fixed form submissions and improved css

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,6 @@
 import { Program,ProgramForm, FilterProps, ApplFilterProps, BenefFilterProps, ProfileFilter, ProgramDetails, ApplicationDetails, BenefitDetails, ProgramRegistrantInfo,} from '@types';
 import { prefixBaseApiPath } from './path';
-// import { unstable_noStore as noStore } from 'next/cache';
 
-
-  // const res = await fetch(`http://localhost:8000/programs?program_id=${program}&page=${currentPage}`);
 
 export async function fetchProgramDetails(): Promise<ProgramDetails[]> {
 
@@ -14,18 +11,6 @@ export async function fetchProgramDetails(): Promise<ProgramDetails[]> {
   const data: ProgramDetails[] = await res.json();
   return data;
 }
-
-// export async function fetchProgramDetails(page = 1, itemsPerPage = 10) {
-//   const res = await fetch(`${prefixBaseApiPath(`/programdetails`)}?page=${page}&limit=${itemsPerPage}`);
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data');
-//   }
-//   const data = await res.json();
-//   return {
-//     programs: data.items, // Assuming 'items' contains the list of programs
-//     total: data.total   // Assuming 'total' is provided by your API
-//   };
-// }
 
 export async function fetchApplicationDetails(): Promise<ApplicationDetails[]> {
 
@@ -47,14 +32,6 @@ export async function fetchBenefitDetails(): Promise<BenefitDetails[]> {
   return data;
 }
 
-// export async function fetchBenefitDetails(page: number = 1, pageSize: number = 10): Promise<BenefitDetails[]> {
-//   const res = await fetch(prefixBaseApiPath(`/benefitdetails?page=${page}&pageSize=${pageSize}`));
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data');
-//   }
-//   const data: BenefitDetails[] = await res.json();
-//   return data;
-// }
 
 export async function fetchPrograms(): Promise<Program[]> {
 
@@ -102,7 +79,7 @@ export async function SubmitForm(programId: number, submissionData: any): Promis
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(submissionData),
+    body: JSON.stringify({ program_registrant_info: submissionData }),
   });
 
   if (!res.ok) {
