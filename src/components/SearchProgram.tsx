66d0a237681b05d32230b83228576d0fa5440f97
shivamg9 <shivamg@iitkgp@ac.in>
2024-03-28 13:15:26 +0530

    changed application status to applied for inprogress

--- src/components/SearchProgram.tsx
+++ src/components/SearchProgram.tsx
@@ -1,18 +1,46 @@
 "use client";
-import {useState, Fragment} from "react";
+import {useState, Fragment, useEffect} from "react";
 import {Combobox, Transition} from "@headlessui/react";
-import {SearchProgramsProps} from "../types";
+import {Program, SearchProgramsProps} from "../types";
 
 const SearchProgram = ({program, setProgram}: SearchProgramsProps) => {
   const [query, setQuery] = useState("");
-  const programs = ["p-1", "p-2", "p-3", "p-4"];
+  // const programs = ["p-1", "p-2", "p-3", "p-4"];
+  const [programs, setPrograms] = useState<Program[]>([]);
 
-  const filteredPrograms =
-    query === ""
-      ? programs
-      : programs.filter((item) =>
-          item.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""))
-        );
+  const fetchProgramsByKeyword = async (keyword: string) => {
+    try {
+      const response = await fetch(`discovery?keyword=${encodeURIComponent(keyword)}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Error fetching programs:", error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    if (query.length > 0) {
+      const loadPrograms = async () => {
+        const fetchedPrograms = await fetchProgramsByKeyword(query);
+        setPrograms(fetchedPrograms);
+      };
+      loadPrograms();
+    } else {
+      setPrograms([]); // Reset the programs list when query is empty
+    }
+  }, [query]);
+
+  // const filteredPrograms = programs.map(program => program.name); // Assuming the API returns programs with a 'name' property
+
+  // const filteredPrograms =
+  //   query === ""
+  //     ? programs
+  //     : programs.filter((item) =>
+  //         item.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""))
+  //       );
   return (
     <div className=" relative w-full flex justify-end">
       <div className="w-1/2">
@@ -56,25 +84,26 @@ const SearchProgram = ({program, setProgram}: SearchProgramsProps) => {
                 className="absolute mt- max-h-60 w-full overflow-auto rounded-md bg-white-100 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
                 static
               >
-                {filteredPrograms.length === 0 && query !== "" ? (
+                {programs.length === 0 && query !== "" ? (
                   <Combobox.Option value={query} className=" cursor-default select-none py-2 pl-10 pr-4">
                     Create &quot;{query}&quot;
                   </Combobox.Option>
                 ) : (
-                  filteredPrograms.map((item) => (
+                  programs.map((program) => (
                     <Combobox.Option
-                      key={item}
+                      key={program.id}
+                      value={program.name}
                       className={({active}) =>
                         `hover:bg-white opacity-90 z-10 relative cursor-default bg-blue-100 select-none py-2 pl-10 pr-4 ${
                           active ? "bg-primary-blue text-black" : "text-gray-900"
                         }`
                       }
-                      value={item}
+                      // value={item}
                     >
                       {({selected, active}) => (
                         <>
                           <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
-                            {item}
+                            {program.name}
                           </span>
 
                           {/* Show an active blue background color if the option is selected */}
