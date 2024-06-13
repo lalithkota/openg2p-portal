1866242685c784626047f5a6021a0629e19c0487
shivamg9 <shivamg@iitkgp@ac.in>
2024-06-13 22:52:52 +0530

    G2P-2515: Breadcrumbs arrows now point correctly

--- src/app/[locale]/programs/page.tsx
+++ src/app/[locale]/programs/page.tsx
@@ -404,7 +404,12 @@ export default function ProgrmPage({
           >
             {" " + t("Home") + " "}
           </Link>
-          <svg xmlns="http://www.w3.org/2000/svg" height="0.8em" viewBox="0 0 320 512">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="0.8em"
+            viewBox="0 0 320 512"
+            style={{transform: "rotate(180deg)"}}
+          >
             <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
           </svg>
           <p
